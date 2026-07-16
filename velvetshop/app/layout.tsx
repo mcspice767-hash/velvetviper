b32dc1d7eb0ee14ad0e34ca2ac1b52c47a439f9c
VelvetViper <reptilelizard48@gmail.com>
2026-07-16 09:42:53 +0100

    redeploy all fixes

--- velvetshop/app/layout.tsx
+++ velvetshop/app/layout.tsx
@@ -24,46 +24,22 @@ export default function RootLayout({
         {children}
 
         <Script
-          id="smartsupp"
-          strategy="afterInteractive"
+          id="smartsupp-config"
+          strategy="beforeInteractive"
           dangerouslySetInnerHTML={{
             __html: `
               var _smartsupp = _smartsupp || {};
               _smartsupp.key = '206a5b2a55dc18f579751d4bc0238d81b164f5ed';
-              window.smartsupp||(function(d) {
-                var s,c,o=smartsupp=function(){ o._.push(arguments)};o._=[];
-                s=d.getElementsByTagName('script')[0];c=d.createElement('script');
-                c.type='text/javascript';c.charset='utf-8';c.async=true;
-                c.src='https://www.smartsuppchat.com/loader.js?';
-                s.parentNode.insertBefore(c,s);
-              })(document);
-
-              window.openSmartsuppChat = function() {
-  try {
-    // Method 1: Click the widget button directly
-    var widgetBtn = document.querySelector(
-      '#smartsupp-widget-container button, #chat-application button, .smartsupp-widget-open, [class*="smartsupp"] button'
-    );
-    if (widgetBtn) {
-      widgetBtn.click();
-      return;
-    }
-
-    // Method 2: Find smartsupp iframe
-    var iframe = document.querySelector('iframe[src*="smartsupp"]');
-    if (iframe) {
-      iframe.style.display = 'block';
-      return;
-    }
-
-    // Method 3: WhatsApp fallback
-    window.open('https://wa.me/YOURPHONENUMBER', '_blank');
-
-  } catch(e) {
-    window.open('https://wa.me/YOURPHONENUMBER', '_blank');
-  }
-};
             `,
+          }}
+        />
+
+        <Script
+          id="smartsupp-loader"
+          strategy="afterInteractive"
+          src="https://www.smartsuppchat.com/loader.js?"
+          onLoad={() => {
+            console.log('Smartsupp loaded successfully');
           }}
         />
         <noscript>
